227d54cd832018c7c1d703d2a8178873b97044f8
saljaoui <soufian2022ff@gmail.com>
2026-03-10 02:38:07 +0000

    `Refactor infrastructure and services to use Spring WebFlux and Spring Cloud Gateway`

---
+++ services/user-service/src/main/java/com/buy01/users/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.buy01.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/ping", "/actuator/health").permitAll()
+                        .anyRequest().permitAll()); // just db mn b3d o nkado 
+
+        return http.build();
+    }
+
+}
