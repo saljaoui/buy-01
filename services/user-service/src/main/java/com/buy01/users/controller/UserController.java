490260f11824132d5ee43b66c17bf524e6101cf4
saljaoui <soufian2022ff@gmail.com>
2026-03-10 02:38:07 +0000

    `Refactor infrastructure and services to use Spring WebFlux and Spring Cloud Gateway`

--- services/user-service/src/main/java/com/buy01/users/controller/UserController.java
+++ services/user-service/src/main/java/com/buy01/users/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.buy01.users.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     @GetMapping("/users/ping")
