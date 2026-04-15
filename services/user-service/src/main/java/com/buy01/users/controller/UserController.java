99bc3264fa0c5385e96b25bb7d447234ab343c28
saljaoui <soufian2022ff@gmail.com>
2026-04-15 16:42:27 +0100

    remove warnnig

--- services/user-service/src/main/java/com/buy01/users/controller/UserController.java
+++ services/user-service/src/main/java/com/buy01/users/controller/UserController.java
@@ -6,7 +6,6 @@ import com.buy01.users.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
