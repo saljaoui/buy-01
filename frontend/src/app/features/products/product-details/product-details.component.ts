0c4c7e019a479be2ecd6b61ea915247aa44d10b9
saljaoui <soufian2022ff@gmail.com>
2026-04-22 16:41:03 +0100

    `Added NavbarComponent to ProductDetailsComponent and ProductListComponent, removed navigation bar code from both components`

--- frontend/src/app/features/products/product-details/product-details.component.ts
+++ frontend/src/app/features/products/product-details/product-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../../shared/footer/footer.component';
+import { NavbarComponent } from '../../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-product-details',
-  imports: [FooterComponent],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './product-details.html',
   styleUrl: './product-details.scss',
 })
