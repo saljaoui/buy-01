6dbf344d150fb1b6d39c340aa8aa4f2bad572f93
saljaoui <soufian2022ff@gmail.com>
2026-04-22 16:17:52 +0100

    `Removed footer component and styles`

--- frontend/src/app/shared/footer/footer.component.ts
+++ frontend/src/app/shared/footer/footer.component.ts
@@ -4,21 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-footer',
   imports: [RouterLink],
-  template: `
-    <footer class="footer-strip">
-      <div class="container">
-        <div class="footer-inner">
-          <div class="footer-brand">Buy<em>01</em></div>
-          <div class="footer-links">
-            <a href="#">Collections</a>
-            <a href="#">Seller guidelines</a>
-            <a href="#">Support</a>
-            <a href="#">Privacy</a>
-          </div>
-        </div>
-      </div>
-    </footer>
-  `,
-  styleUrls: ['./footer.component.scss']
+  templateUrl: './footer.html',
+  styleUrls: ['./footer.scss']
 })
 export class FooterComponent {}
